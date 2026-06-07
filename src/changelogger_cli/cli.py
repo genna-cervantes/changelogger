ce6a0c1b69241494deddb2e84fe93a8ed80939f9
genna-cervantes <gennacervantes9@gmail.com>
2026-06-08 00:21:50 +0800

    feat accept cli arguments

--- src/changelogger_cli/cli.py
+++ src/changelogger_cli/cli.py
@@ -1,15 +1,29 @@
 import subprocess
 from changelogger_cli.ai import ai_format_messages
 from changelogger_cli.types import CommitObject
+import argparse
 
-# accept 2 branches
 # accept repository/directory
-# flag to show leftover commits
 def main() -> None:
-    subprocess.run(['git', 'fetch', 'origin', 'releasing'], cwd='/Users/gennacervantes/Documents/oboda-app')
-    subprocess.run(['git', 'fetch', 'origin', 'develop'], cwd='/Users/gennacervantes/Documents/oboda-app')
     
-    gitLog = subprocess.run(['git', 'log', 'releasing..develop', '--pretty=format:"%h %s"'], 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--from')
+    parser.add_argument('-t', '--to')
+    parser.add_argument('-e', '--enhance', action='store_true')
+    parser.add_argument('-l', '--show-leftover', action='store_true')
+    
+    args = parser.parse_args()
+    fromBranch = args.__dict__['from'] if args.__dict__['from'] else 'develop'
+    toBranch = args.__dict__['to'] if args.__dict__['to'] else 'releasing'
+    isEnhance = args.__dict__['enhance'] if args.__dict__['enhance'] else False
+    is_show_leftover = args.__dict__['show_leftover'] if args.__dict__['show_leftover'] else False
+    
+    print(f"Generating changelog from {fromBranch} to {toBranch}...")
+    
+    subprocess.run(['git', 'fetch', 'origin', fromBranch], cwd='/Users/gennacervantes/Documents/oboda-app')
+    subprocess.run(['git', 'fetch', 'origin', toBranch], cwd='/Users/gennacervantes/Documents/oboda-app')
+    
+    gitLog = subprocess.run(['git', 'log', f'{toBranch}..{fromBranch}', '--pretty=format:"%h %s"'], 
                             capture_output=True, text=True, cwd='/Users/gennacervantes/Documents/oboda-app')
     
     commits = gitLog.stdout.splitlines()
@@ -33,7 +47,8 @@ def main() -> None:
             'hash': commitHash,
             'message': commitMessage,
             'ticket': commitTicket,
-            'ticketMessage': commitTicketMessage
+            'ticketMessage': commitTicketMessage,
+            'enhancedTicketMessage': ''
         })
 
     primeCommitObjects: list[CommitObject] = []
@@ -60,11 +75,14 @@ def main() -> None:
                 case _:
                     leftoverCommitObjects.append(commitObject)
 
-    enhancedCommitObjects = ai_format_messages(primeCommitObjects + activationCommitObjects + aiCommitObjects + csoCommitObjects + krnlCommitObjects)
+    enhancedCommitObjects: list[CommitObject] = primeCommitObjects + activationCommitObjects + aiCommitObjects + csoCommitObjects + krnlCommitObjects
+    if isEnhance:
+        enhancedCommitObjects = ai_format_messages(primeCommitObjects + activationCommitObjects + aiCommitObjects + csoCommitObjects + krnlCommitObjects)
     
     for commitObject in enhancedCommitObjects:
         print(f"{commitObject['ticket']}: {commitObject['ticketMessage']} -> {commitObject['enhancedTicketMessage']}")
         
-    print("\nLeftover commits:")
-    for commitObject in leftoverCommitObjects:
-        print(f"{commitObject['ticket']}: {commitObject['ticketMessage']} -> No enhancement (unrecognized board)")
+    if is_show_leftover:
+        print("\nLeftover commits:")
+        for commitObject in leftoverCommitObjects:
+            print(f"{commitObject['ticket']}: {commitObject['ticketMessage']} -> No enhancement (unrecognized board)")
